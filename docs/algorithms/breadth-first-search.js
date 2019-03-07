39aadf85b5531367e99ac72b44f1227b70d49e65
RJ Salvador <rjsalvadorr@gmail.com>
2019-03-07 17:11:40 -0500

    yep

--- docs/algorithms/breadth-first-search.js
+++ docs/algorithms/breadth-first-search.js
@@ -1,5 +1,35 @@
 import Queue from "../lists/queue";
 
-function breadthFirstSearch(rootNode) {}
+/**
+ * Initializes callback functions for breadth-first search
+ * @function
+ * @param {Object} callbacks - callbacks to initialize
+ * @returns {function} the initiated callback
+ */
+function bfsInitCallbacks(callbacks = {}) {
+  const initiatedCallback = callbacks;
+
+  const stubCallback = () => {};
+  const defaultAllowTraversal = () => true;
+
+  initiatedCallback.allowTraversal =
+    callbacks.allowTraversal || defaultAllowTraversal;
+  initiatedCallback.enterNode = callbacks.enterNode || stubCallback;
+  initiatedCallback.leaveNode = callbacks.leaveNode || stubCallback;
+
+  return initiatedCallback;
+}
+
+/**
+ * Implementation of a breadth-first search on a binary tree
+ * @function
+ * @param {Object} rootNode - binary tree node
+ * @param {Object} origCallbacks - original callbacks
+ * @returns {number[]} a sorted version of the given array
+ */
+function breadthFirstSearch(rootNode, origCallbacks) {
+  const callbacks = bfsInitCallbacks(origCallbacks);
+  const nodeQueue = new Queue();
+}
 
 export default breadthFirstSearch;
